40b70dcc5e83a288d0a2c5dc46a423907cdca871
鄧宇辰 <mejheek@gmail.com>
2024-03-01 15:58:28 +0800

    更新

---
+++ O.py
@@ -0,0 +1,6 @@
+import turtle
+repeat = 0
+while repeat <= 360:
+    turtle.forward(1)
+    turtle.right(1)
+    repeat += 1
---
+++ spiral.py
@@ -0,0 +1,11 @@
+import turtle
+
+length = 50
+angle = 130
+counter = 0
+
+while counter < 40:
+    turtle.forward(length)
+    turtle.left(angle)
+    
+    counter = counter + 1
